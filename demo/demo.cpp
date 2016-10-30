ac45306e6c0a22c680c57652427ed222758d5a4c
WSPOLSL <1806010263@qq.com>
2016-10-30 19:19:39 +0800

    233
    2334444

---
+++ demo/demo.cpp
@@ -0,0 +1,30 @@
+// demo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+//#include<SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	RenderWindow window(VideoMode(200, 200), "SFML works!");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+	return 0;
+}
+
